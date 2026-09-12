96ffc893a7f31217392f987217b95272793cf0dc
Tirth S Pandya <teamfznltd@gmail.com>
2026-09-12 09:24:27 +0530

    complete navbar basic

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -2,12 +2,14 @@ interface Props {
     imgUrl: any
     title: string
     href: string
+    height: number
+    width: number
 }
 function Icons (props: Props) {
     return (
     <>
-    <div className="flex flex-row pr-10">
-        <img src={props.imgUrl}/>
+    <div className="flex flex-col pr-10 items-center">
+        <a href={props.href}><img className="ml-2" src={props.imgUrl} height={props.height} width={props.width}/></a>
         <a href={props.href}>{props.title}</a>
     </div>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,19 @@
 import Icons from "./Icons";
-import { GoHomeFill } from "react-icons/go";
+import { House } from 'lucide-react';
 function Navbar () {
     return (
     <>
-    <div className="flex justify-between px-80 py-1">
+    <div className="flex justify-between px-80 py-1 border-b-1">
         <div className="flex flex-row">
-            <img className="mx-5" src="./src/assets/logo.png" height={40} width={40}/>
+            <img className="mx-5" src="./src/assets/logo.png" height={50} width={50}/>
             <input className="px-7 h-8 rounded-3xl mt-2" type="text" placeholder="Search"/>
         </div>
         <div className="flex flex-row mt-2">
-            <Icons title={"Home"} imgUrl={<GoHomeFill />} href="#"/>
-            <Icons title={"Home"} imgUrl={""} href="#"/>
-            <Icons title={"Home"} imgUrl={""} href="#"/>
-            <Icons title={"Home"} imgUrl={""} href="#"/>
+            <Icons title={"Home"} imgUrl={"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWhvdXNlIj48cGF0aCBkPSJNMTUgMjF2LThhMSAxIDAgMCAwLTEtMWgtNGExIDEgMCAwIDAtMSAxdjgiLz48cGF0aCBkPSJNMyAxMGEyIDIgMCAwIDEgLjcwOS0xLjUyOGw3LTZhMiAyIDAgMCAxIDIuNTgyIDBsNyA2QTIgMiAwIDAgMSAyMSAxMHY5YTIgMiAwIDAgMS0yIDJINWEyIDIgMCAwIDEtMi0yeiIvPjwvc3ZnPg=="} height={23} width={23} href="#"/>
+            <Icons title={"My Network"} imgUrl={"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXVzZXJzIj48cGF0aCBkPSJNMTYgMjF2LTJhNCA0IDAgMCAwLTQtNEg2YTQgNCAwIDAgMC00IDR2MiIvPjxwYXRoIGQ9Ik0xNiAzLjEyOGE0IDQgMCAwIDEgMCA3Ljc0NCIvPjxwYXRoIGQ9Ik0yMiAyMXYtMmE0IDQgMCAwIDAtMy0zLjg3Ii8+PGNpcmNsZSBjeD0iOSIgY3k9IjciIHI9IjQiLz48L3N2Zz4="} height={23} width={23} href="#"/>
+            <Icons title={"Jobs"} imgUrl={"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWJyaWVmY2FzZS1idXNpbmVzcyI+PHBhdGggZD0iTTEyIDEyaC4wMSIvPjxwYXRoIGQ9Ik0xNiA2VjRhMiAyIDAgMCAwLTItMmgtNGEyIDIgMCAwIDAtMiAydjIiLz48cGF0aCBkPSJNMjIgMTNhMTguMTUgMTguMTUgMCAwIDEtMjAgMCIvPjxyZWN0IHdpZHRoPSIyMCIgaGVpZ2h0PSIxNCIgeD0iMiIgeT0iNiIgcng9IjIiLz48L3N2Zz4="} height={23} width={23} href="#"/>
+            <Icons title={"Messages"} imgUrl={"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1lc3NhZ2VzLWNpcmNsZSI+PHBhdGggZD0iTTE5Ljk1IDEwLjA1YTcgNyAwIDAxMS40MTIgNy44NzIgMSAxIDAgMDAtLjA1OC43ODdsLjY3NSAyLjA4OWExIDEgMCAwMS0xLjIzNiAxLjE2OGwtMi4xNTUtLjYzMWExIDEgMCAwMC0uNzQ1LjA2IDcgNyAwIDAxLTcuNzkzLTEuNDQ1Ii8+PHBhdGggZD0iTTIuNjk2IDEyLjcwOGExIDEgMCAwMC0uMDU4LS43ODUgNyA3IDAgMTEzLjUxOCAzLjQ3MyAxIDEgMCAwMC0uNzQ0LS4wNjFsLTIuMTU1LjYzYTEgMSAwIDAxLTEuMjM2LTEuMTY3eiIvPjwvc3ZnPg=="} height={23} width={23} href="#"/>
+            <Icons title={"Notifications"} imgUrl={"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWJlbGwiPjxwYXRoIGQ9Ik0xMC4yNjggMjFhMiAyIDAgMCAwIDMuNDY0IDAiLz48cGF0aCBkPSJNMy4yNjIgMTUuMzI2QTEgMSAwIDAgMCA0IDE3aDE2YTEgMSAwIDAgMCAuNzQtMS42NzNDMTkuNDEgMTMuOTU2IDE4IDEyLjQ5OSAxOCA4QTYgNiAwIDAgMCA2IDhjMCA0LjQ5OS0xLjQxMSA1Ljk1Ni0yLjczOCA3LjMyNiIvPjwvc3ZnPg=="} height={23} width={23} href="#"/>
         </div>
     </div>
     </>
