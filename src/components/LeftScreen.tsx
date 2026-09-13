01c1b6dedad5da46e52c15f324641f0fba95c624
Tirth S Pandya <teamfznltd@gmail.com>
2026-09-13 12:00:28 +0530

    add premium section

--- src/components/LeftScreen.tsx
+++ src/components/LeftScreen.tsx
@@ -1,10 +1,12 @@
+import Premium from "./Premium";
 import ProfileCard from "./ProfileCard";
 
 function LeftScreen(){
     return (
         <>
-        <div>
+        <div className="flex flex-col">
             <ProfileCard img1={"https://i.pinimg.com/736x/14/ca/04/14ca04dca6e0ff16418f58f0c228bfdd.jpg"} img2={"https://i.pinimg.com/736x/30/2d/6b/302d6bfe12944edd316a4954b82faac3.jpg"} name={"Gadha Sala"} desc={"Top 800 Qualified"} state={"Gujarat"} currentPosi={"Founding Engineer"}/>
+            <Premium/>
         </div>
         </>
     )
---
+++ src/components/Premium.tsx
@@ -0,0 +1,14 @@
+function Premium () {
+    return (
+        <>
+        <div className="border-2 p-5 rounded-2xl mt-2">
+            <p className="text-sm">Boost your career with exclusive <br/> tools</p>
+            <div className="flex mt-2">
+                <img src="https://i.pinimg.com/736x/64/5d/81/645d81811aac8811a89dcd905712f164.jpg" height={20} width={20}/>
+                <p className="px-2 font-bold">Try premium for ₹0</p>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Premium;
