c6d2b24f62a63efa0cda7f2e54c255480f6d79a1
Tirth S Pandya <teamfznltd@gmail.com>
2026-09-14 11:00:56 +0530

    add news component

--- src/components/RightScreen.tsx
+++ src/components/RightScreen.tsx
@@ -1,8 +1,16 @@
 function RightScreen () {
     return (
         <>
-        <div>
-            Right screen
+        <div className="border-2 p-5 rounded-2xl ">
+            <div>
+                <div className="flex flex-row p-2">
+                    <p className="mr-29 font-bold text-2xl">Linkedin News</p>
+                    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW5ld3NwYXBlciI+PHBhdGggZD0iTTE1IDE4aC01Ii8+PHBhdGggZD0iTTE4IDE0aC04Ii8+PHBhdGggZD0iTTQgMjJoMTZhMiAyIDAgMCAwIDItMlY0YTIgMiAwIDAgMC0yLTJIOGEyIDIgMCAwIDAtMiAydjE2YTIgMiAwIDAgMS00IDB2LTlhMiAyIDAgMCAxIDItMmgyIi8+PHJlY3Qgd2lkdGg9IjgiIGhlaWdodD0iNCIgeD0iMTAiIHk9IjYiIHJ4PSIxIi8+PC9zdmc+" height={12} width={12}/>
+                </div>
+                <div className="p-2">
+                    <p>The sports conversations to join this...</p>
+                </div>
+            </div>
         </div>
         </>
     )
